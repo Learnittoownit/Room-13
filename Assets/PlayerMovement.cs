d13054b6f905f1bf7161d823967e3bb2db77ab9f
Alreem-ai <aldossaryalreem@gmail.com>
2025-06-26 04:07:16 +0300

    Camera Script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
+using Unity.Hierarchy;
 
 public class PlayerMovement : MonoBehaviour
 {
     public bool jump;
     public float JumpSpeed;
     public float PlayerSpeed;
+    public GameObject PlayerCamera;
     public Rigidbody PlayerRB;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
-   
+    // Update is called once per frame
     void Update()
     {
+
+
         float horoizontal = Input.GetAxis("Horizontal") * PlayerSpeed;
         float vertical = Input.GetAxis("Vertical") * PlayerSpeed;
 
@@ -25,13 +31,31 @@ public class PlayerMovement : MonoBehaviour
             jump = false;
         }
 
-        void OnCollisionEnter(Collision collision)
-        {
-
-
-            //Â‰« ›Ì «‰Â Ì «ﬂœ «‰ «·«⁄» „ÊÃÊœ ⁄·Ï «·«—÷ Ê·« ·«
-            jump = true;
-        }
+        MoveCamera();
 
     }
+
+    float rotationCam = 0;
+
+    void MoveCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(new Vector3(0, mouseX, 0));
+
+        float mouseY = Input.GetAxis("Mouse Y");
+        rotationCam -= mouseY;
+
+        PlayerCamera.transform.localRotation = Quaternion.Euler(rotationCam, 0, 0);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+
+        //Â‰« ›Ì «‰Â Ì «ﬂœ «‰ «·«⁄» „ÊÃÊœ ⁄·Ï «·«—÷ Ê·« ·«
+        jump = true;
+    }
+
 }
+
