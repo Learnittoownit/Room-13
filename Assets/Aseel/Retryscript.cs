d7a3889f1d107291308cb94845e5c8048c78f0ef
Aseel-63 <asukkar33@gmail.com>
2025-06-29 19:53:09 +0300

    UI
    A

---
+++ Assets/Aseel/Retryscript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryManager : MonoBehaviour
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void ReloadGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
+
